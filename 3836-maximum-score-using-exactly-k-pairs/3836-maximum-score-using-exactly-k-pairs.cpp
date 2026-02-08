d0fd465e4859d4ab920a2ceceb7aaafde136adf6
Vedant Khasbage <vedantkhasbage2005@gmail.com>
2026-02-08 23:09:08 +0530

    Time: 717 ms (66.01%), Space: 57.5 MB (95.13%) - LeetHub

--- 3836-maximum-score-using-exactly-k-pairs/3836-maximum-score-using-exactly-k-pairs.cpp
+++ 3836-maximum-score-using-exactly-k-pairs/3836-maximum-score-using-exactly-k-pairs.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
 long long dp[101][101][101];
-const long long NEG = LLONG_MIN / 2;
 long long solve(vector<int>&arr1,vector<int>&arr2,int k,int i,int j){
       if(arr1.size() - i < k || arr2.size() - j < k)
-        return NEG;
+        return -1e18;
+
 
     if(i>=arr1.size() || j>=arr2.size()){
         if(k==0) return 0;
-        return NEG;
+        return -1e18;
     }
     if(k==0) return 0;
     
-    long long ans=NEG;
+    long long ans=-1e18;
     
-    if(dp[i][j][k]!=NEG) return dp[i][j][k];
+    if(dp[i][j][k]!=-1e18) return dp[i][j][k];
     ans=max(ans,solve(arr1,arr2,k,i+1,j));
     ans=max(ans,solve(arr1,arr2,k,i,j+1));
+    ans=max(ans,solve(arr1,arr2,k,i+1,j+1));
     ans=max(ans,((long long)arr1[i]*(long long)arr2[j])+solve(arr1,arr2,k-1,i+1,j+1));
     return dp[i][j][k]=ans; 
     
@@ -25,7 +26,7 @@ long long solve(vector<int>&arr1,vector<int>&arr2,int k,int i,int j){
         
         for(int i=0;i<=100;i++){
             for(int j=0;j<=100;j++){
-                for(int l=0;l<=100;l++) dp[i][j][l]=NEG;
+                for(int l=0;l<=100;l++) dp[i][j][l]=-1e18;
             }
         }
         return solve(arr1,arr2,k,0,0);
