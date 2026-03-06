d6cf6fd43260362ca1418b41c436fefe8bdd2493
Vedant Khasbage <vedantkhasbage2005@gmail.com>
2026-03-06 08:24:36 +0530

    Time: 0 ms (100%), Space: 8.3 MB (46.93%) - LeetHub

---
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+       
+         int i=0;
+         while(i<s.size() && s[i]=='1') i++;
+       
+       
+         while(i<s.size()){
+            if(s[i]=='1') return false;
+            i++;
+         }
+
+         return true;
+    }
+};
