8846fd3f78c4c02da710ab63e1ec9efc22c35d8b
Vedant Khasbage <vedantkhasbage2005@gmail.com>
2026-03-11 21:19:58 +0530

    Time: 0 ms (100%), Space: 7.9 MB (26.22%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-int power(int a,int b){
-    if(b==0) return 1;
-    
-    int half=power(a,b/2);
-    if(b%2==0) return half*half;
-    return half*half*a; 
-}
-    int bitwiseComplement(int n) {
-          if(n==0) return 1;
-        string bin=""; 
-       
-       while(n>0){
-         int rem=n%2;
-         bin+=to_string(rem);
-         n/=2;
-       }
-      int ans=0;
-      string comp="";
-      for(int i=0;i<bin.size();i++){
-        if(bin[i]=='0') comp+='1';
-        else comp+='0';
-      }
-      for(int i=0;i<bin.size();i++){
-        ans=ans+((comp[i]-'0')*(power(2,i)));
-      } 
 
-    cout << bin << endl;
-       return ans;
+    int bitwiseComplement(int n) {
+        
+        int mask=0;
+       int dum=n;
+     int idx=0;
+     int hi=0;
+     while(dum>0){
+        if(dum%2!=0){
+            hi=idx;
+        }
+        idx++;
+        dum>>=1;
+     }
+     mask=(1<<(hi+1))-1;
+     return n^mask;
+
+    //    return ans;
     }
 };
