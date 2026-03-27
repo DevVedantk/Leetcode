4b8b3fe1cb5c408ac8fa81da1e18941282053615
Vedant Khasbage <vedantkhasbage2005@gmail.com>
2026-03-27 08:19:12 +0530

    Time: 0 ms (100%), Space: 32.7 MB (26.09%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& org, int k) {
+        vector<vector<int>>mat=org;
+        int flip=k%mat[0].size();
+         
+        for(int i=0;i<flip;i++){
+             
+             for(int a=0;a<mat.size();a++){
+                if(a%2==0){
+                    for(int col=0;col<mat[0].size()-1;col++){
+                        swap(mat[a][col],mat[a][col+1]);
+                    }
+                } else{
+                   for(int col=mat[0].size()-1;col>=1;col--){
+                        swap(mat[a][col],mat[a][col-1]);
+                    }  
+                }   
+
+             }
+        }
+
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                if(mat[i][j]!=org[i][j]) return false;
+            }
+        }  
+
+        return true;
+
+
+    }
+};
