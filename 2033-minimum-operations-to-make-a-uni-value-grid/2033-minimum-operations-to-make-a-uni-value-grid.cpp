12b7c5c96a48fc767d5b2f2835a2dced07fc7b36
Vedant Khasbage <vedantkhasbage2005@gmail.com>
2026-04-28 15:59:16 +0530

    Time: 55 ms (14.82%), Space: 90.1 MB (34.57%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -8,18 +8,14 @@ public:
          for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[i].size();j++){
                  arr.push_back(grid[i][j]);
-                //  if(same==-1) same=grid[i][j];
-                //  else if(same!=-1 && grid[i][j]!=same) dup=true;
             }
          }
-    //   if(!dup) return 0;
 
          sort(arr.begin(),arr.end());
          int n=arr.size();
          int mid=arr[n/2];
         
         int ans=0;
-        cout << mid << endl;
          for(int i=0;i<arr.size();i++){
             int df=abs(arr[i]-mid);
             if(df%x!=0) return -1;
