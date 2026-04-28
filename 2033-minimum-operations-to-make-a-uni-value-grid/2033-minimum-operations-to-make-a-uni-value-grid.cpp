c246fc883d591cafec24c0b8065d386eb37a9d39
Vedant Khasbage <vedantkhasbage2005@gmail.com>
2026-04-28 15:58:15 +0530

    Time: 51 ms (25.93%), Space: 90.1 MB (34.57%) - LeetHub

---
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        bool check=false;
+         vector<int>arr;
+         int same=-1;
+         bool dup=false;
+         for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[i].size();j++){
+                 arr.push_back(grid[i][j]);
+                 if(same==-1) same=grid[i][j];
+                 else if(same!=-1 && grid[i][j]!=same) dup=true;
+                //  if(grid[i][j]%x!=0) return -1;
+            }
+         }
+      if(!dup) return 0;
+
+         sort(arr.begin(),arr.end());
+         int n=arr.size();
+         int mid=arr[n/2];
+        
+        int ans=0;
+        cout << mid << endl;
+         for(int i=0;i<arr.size();i++){
+            int df=abs(arr[i]-mid);
+            if(df%x!=0) return -1;
+            ans=ans+(df/x);
+         }
+
+         return ans;
+          
+    }
+};
