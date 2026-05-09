28b7aadad7e9fa42cd7d76cd5389b944deec5ea9
Vedant Khasbage <vedantkhasbage2005@gmail.com>
2026-05-09 18:34:18 +0530

    Time: 79 ms (73.69%), Space: 44.3 MB (81.25%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        
         vector<int>dist(n+1,INT_MAX);
         vector<vector<pair<int,int>>>adj(n+1);
 
         for(int i=0;i<times.size();i++){
-            int u=times[i][0],v=times[i][1],wt=times[i][2];
-
-            adj[u].push_back({v,wt});
+            adj[times[i][0]].push_back({times[i][1],times[i][2]});
         }
 
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        
         dist[k]=0;
-       priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-       pq.push({0,k});
+        //dist , node
+        pq.push({0,k});
+        
+        while(pq.size()>0){
+            auto [dis,node]=pq.top();
+            pq.pop();
 
-       while(pq.size()>0){
-       auto [dis,node]=pq.top();
-       pq.pop();
+            if(dist[node]>dis) continue;
 
-       if(dis>dist[node]) continue;
+            for(auto [child,wt]:adj[node]){
+                if(dist[child]>wt+dis){
+                    dist[child]=wt+dis;
+                    pq.push({dist[child],child});
+                }
+            }
+        }
 
-       for(auto [child,wt]:adj[node]){
-          if(dist[child]>dis+wt){
-             dist[child]=dis+wt;
-             pq.push({dist[child],child});
-          }
-       }
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if(dist[i]==INT_MAX) return -1;
+            ans=max(ans,dist[i]);
+        }
 
-       } 
-       
-       int ans=INT_MIN;
-       for(int i=1;i<=n;i++){
-        ans=max(ans,dist[i]);
-       }
-
-       return ans==INT_MAX ? -1 : ans;
-
+        return ans;
     }
 };
