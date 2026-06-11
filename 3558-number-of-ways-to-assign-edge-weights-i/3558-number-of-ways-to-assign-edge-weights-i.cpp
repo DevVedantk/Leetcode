6f91e84b0a4a77b20da5dea8886537b23e6070d3
Vedant Khasbage <vedantkhasbage2005@gmail.com>
2026-06-11 08:32:13 +0530

    Time: 298 ms (50.34%), Space: 339.1 MB (42.28%) - LeetHub

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -1,48 +1,43 @@
 class Solution {
 public:
+vector<int>dist;
+    void dfs(int node,int par,vector<vector<int>>&adj){
+         
+         for(int child:adj[node]){
+            if(child==par) continue;
+            dist[child]=1+dist[node];
+            dfs(child,node,adj);
+         }
 
-vector<int>dep;
-void dfs(int node,int parent,vector<vector<int>>& adj){
+    }
+    int dp[100001][2];
+    int MOD=1e9+7;
+    int solve(int idx,int len,int sum){
+        if(idx>=len){
+            return (sum!=0);
+        }
+       if(dp[idx][sum]!=-1) return dp[idx][sum];
+        int ans=0;
+        ans=(ans+solve(idx+1,len,(sum+1)%2))%MOD;
+        ans=(ans+solve(idx+1,len,(sum+2)%2))%MOD;
 
-       for(int child:adj[node]){
-        if(child==parent) continue;
-        dep[child]=1+dep[node];
-        dfs(child,node,adj);
-       } 
-}
+        return dp[idx][sum]=ans;
+    }
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        int n=edges.size()+1;
+        vector<vector<int>>adj(n+1);
 
-int dp[100001][2];
-int MOD=1e9+7;
-
-int solve(int idx,int n,int sum){
-   if(idx>=n) return (sum==1);
-   int ans=0;
-   
-   if(dp[idx][sum]!=-1) return dp[idx][sum];
-   ans=(ans+solve(idx+1,n,(sum+1)%2))%MOD;
-   ans=(ans+solve(idx+1,n,(sum+2)%2))%MOD;
-   return dp[idx][sum]=ans;
-}
-
-int assignEdgeWeights(vector<vector<int>>& edges) {
-          int n=edges.size()+1;
-   
-   dep.assign(n+1,0);
-          vector<vector<int>>adj(n+1);
-
-          for(int i=0;i<edges.size();i++){
+        for(int i=0;i<edges.size();i++){
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
-          }
+        }
 
-          dfs(1,-1,adj);
-          memset(dp,-1,sizeof(dp));
-          int len=0;
-          for(int i=0;i<dep.size();i++){
-            len=max(len,dep[i]);
-          }
-     
-     int ans=solve(0,len,0);
-          return ans;
+        dist.assign(n+1,0);
+        dfs(1,-1,adj);
+       int len=0;
+       for(int e:dist) len=max(len,e);
+       memset(dp,-1,sizeof(dp));
+
+        return solve(0,len,0);
     }
 };
