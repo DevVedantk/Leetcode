b5ce8a2ce10846cd765236aff11690f1ae9cda69
Vedant Khasbage <vedantkhasbage2005@gmail.com>
2026-06-13 08:52:44 +0530

    Time: 0 ms (100%), Space: 43.7 MB (24.57%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& wt) {
-   map<int,char>mp;
-        char ch='z';
-        for(int i=0;i<=25;i++){
-            mp[i]=ch;
-            ch--;
-        }
-        
         string ans="";
-        int n=words.size();
-        for(int i=0;i<n;i++){
+
+        for(int i=0;i<words.size();i++){
+            string s=words[i];
             int sum=0;
-            for(int j=0;j<words[i].size();j++) sum+=wt[(words[i][j]-'a')];
-            int val=sum%26;
-             ans+=mp[val];
+            for(int j=0;j<s.size();j++){
+                sum+=(wt[s[j]-'a']);
+            }
+            sum=sum%26;
+            ans+=((char)(122-sum));
         }
-   // char c=1+'a';
-        // cout << c << endl;
-        // cout << ans << endl;
+
         return ans;
     }
 };
