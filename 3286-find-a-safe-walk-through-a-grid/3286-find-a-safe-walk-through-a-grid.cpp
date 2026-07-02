029411d3c346bf13e216fdd55c0e4c2e7f9e73a9
Vedant Khasbage <vedantkhasbage2005@gmail.com>
2026-07-02 08:23:13 +0530

    Time: 91 ms (19.72%), Space: 40.2 MB (22.8%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        int n=grid.size(),m=grid[0].size();
-         vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
 
-         dist[0][0]=grid[0][0];
-    priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
-    vector<pair<int,int>>dir={{1,0},{0,1},{-1,0},{0,-1}};
+              int m=grid.size();
+              int n=grid[0].size();
 
-    pq.push({dist[0][0],0,0});
+              vector<vector<int>>dist(m,vector<int>(n,INT_MAX));
+              dist[0][0]=grid[0][0];
 
-    while(pq.size()>0){
-       auto [dis,x,y]=pq.top();
-       pq.pop();
+              set<pair<int,int>>vis;
+              priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
 
-       if(dis>dist[x][y]) continue;
+              vector<pair<int,int>>dir={{1,0},{0,1},{-1,0},{0,-1}};
 
-       for(auto [nx,ny]:dir){
-        int newx=x+nx;
-        int newy=y+ny;
+              pq.push({dist[0][0],{0,0}});
 
-        if(newx>=0 && newy>=0 && newx<n && newy<m && dist[newx][newy]>(dist[x][y]+grid[newx][newy])){
-           dist[newx][newy]=dist[x][y]+grid[newx][newy];
-           pq.push({dist[newx][newy],newx,newy});
-        } 
-       }
-    }
-   
-   cout<<  dist[n-1][m-1] << endl;
-    if((health-dist[n-1][m-1])>=1) return true;
-    return false;
+              while(pq.size()>0){
+                  auto p=pq.top();
+                  pq.pop();
+                 int curr_dis=p.first;
+                  auto [x,y]=p.second;
+                  
+                  auto pair=make_pair(x,y);
+                  if(vis.find(pair)!=vis.end()) continue;
+                  vis.insert(pair);
+
+                  for(int d=0;d<4;d++){
+                    int newx=dir[d].first+x;
+                    int newy=dir[d].second+y;
+
+                    if(newx>=0 && newy>=0 && newx<m && newy<n && vis.find(make_pair(newx,newy))==vis.end()){
+                         dist[newx][newy]=min(dist[newx][newy],curr_dis+grid[newx][newy]);
+                         pq.push({dist[newx][newy],{newx,newy}});
+                    }
+                  }
+              }
+
+              return (health-dist[m-1][n-1])>=1; 
+
     }
 };
