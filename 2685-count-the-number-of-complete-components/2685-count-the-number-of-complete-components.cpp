1bc9bb8612724d1f25a4ec9fc4c3e5b975a17752
Vedant Khasbage <vedantkhasbage2005@gmail.com>
2026-07-11 09:26:11 +0530

    Time: 22 ms (91.21%), Space: 128.8 MB (65.47%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,70 +1,58 @@
 class Solution {
 public:
 vector<bool>vis;
-vector<bool>vis1;
 
-bool check(int node,vector<vector<int>>&adj){
+int solve(int node,vector<vector<int>>&adj){
+     
+     if(adj[node].size()==0) return 0;
 
-    queue<int>q2;
-    q2.push(node);
-    int cnt=0;
-    vis1[node]=true;
-    while(q2.size()>0){
-         int top=q2.front();
-         q2.pop();
-          cnt++;
+     int ans=0;
+     vis[node]=true;
 
-        for(int child:adj[top]){
-            if(vis1[child]==false){
-                vis1[child]=true;
-                q2.push(child);
-               
-            }
-        }
-    }
-    // cout << "size " << cnt << endl;
-    bool flag=true;
+     for(int child:adj[node]){
+         if(!vis[child]){
+            ans=ans+solve(child,adj)+1;
+         }
+     }
 
-    queue<int>q;
-
-    q.push(node);
-    vis[node]=true;
-    while(q.size()>0){
-        int node=q.front();
-        q.pop();
-
-        if((cnt-1)!=adj[node].size()) flag=false;
-
-        for(int child:adj[node]){
-            if(vis[child]==false){
-                
-            vis[child]=true;
-            q.push(child);
-            }
-        }
-    }
-
-    return flag;
+     return ans;        
 }
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vis.assign(n,false);
-        vis1.assign(n,false);
+vector<bool>vis2;
+bool check(int node,int par,int size,vector<vector<int>>&adj){
+    if(adj[node].size()!=size-1) return false;
+    
+    vis2[node]=true;
+    for(int child:adj[node]){
+         if(!vis2[child]){
+        if(!check(child,node,size,adj)) return false;
+         } 
+    }
 
-        vector<vector<int>>adj(n);
-        for(int i=0;i<edges.size();i++){
-            int a=edges[i][0],b=edges[i][1];
-            adj[a].push_back(b);
-            adj[b].push_back(a);
-        }
-    int cnt=0;
-        for(int i=0;i<n;i++){
-            if(vis[i]==false){
-                 if(check(i,adj)){
-                    cnt++;
-                 }
+    return true;
+}
+
+int countCompleteComponents(int n, vector<vector<int>>& edges) {
+         
+         vector<vector<int>>adj(n);
+         for(int i=0;i<edges.size();i++){
+            int u=edges[i][0],v=edges[i][1];
+             
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+         }
+
+         vis.assign(n,false);
+         vis2.assign(n,false);
+         int cnt=0;
+
+         for(int i=0;i<n;i++){
+            if(!vis[i]){
+                int size=solve(i,adj)+1;
+                if(check(i,-1,size,adj)) cnt++;
             }
-        }
+         } 
+       return cnt;
 
-        return cnt;
+
     }
 };
