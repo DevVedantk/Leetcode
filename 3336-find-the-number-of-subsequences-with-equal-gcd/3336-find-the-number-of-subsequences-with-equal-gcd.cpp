2206dc89040d3f5136c359316fda68ff9b5c827c
Vedant Khasbage <vedantkhasbage2005@gmail.com>
2026-07-14 08:11:23 +0530

    Time: 2794 ms (5.21%), Space: 204.2 MB (52.17%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
 int dp[201][201][201];
+vector<vector<int>>gcds;
 int MOD=1e9+7;
 int solve(int idx,int g1,int g2,vector<int>&arr){
      if(idx>=arr.size()){
@@ -10,8 +11,8 @@ int solve(int idx,int g1,int g2,vector<int>&arr){
      int ans=0;
      if(dp[idx][g1][g2]!=-1) return dp[idx][g1][g2];
 
-     ans=(ans+solve(idx+1,gcd(g1,arr[idx]),g2,arr))%MOD;
-     ans=(ans+solve(idx+1,g1,gcd(g2,arr[idx]),arr))%MOD;
+     ans=(ans+solve(idx+1,g1==0 ? arr[idx] : gcds[g1][arr[idx]],g2,arr))%MOD;
+     ans=(ans+solve(idx+1,g1,g2==0 ? arr[idx] : gcds[g2][arr[idx]],arr))%MOD;
      ans=(ans+solve(idx+1,g1,g2,arr))%MOD;
 
      return dp[idx][g1][g2]=ans;
@@ -19,6 +20,14 @@ int solve(int idx,int g1,int g2,vector<int>&arr){
     int subsequencePairCount(vector<int>& arr) {
           
     memset(dp,-1,sizeof(dp));
+          
+          gcds.assign(201,vector<int>(201,1));
+
+          for(int i=0;i<=200;i++){
+            for(int j=0;j<=200;j++){
+                gcds[i][j]=gcd(i,j);
+            }
+          }
           return solve(0,0,0,arr);
     }
 };
